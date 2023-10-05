8cb440fc4fe47a9b6f5178d04cf1e6702f9b63c8
yucedagEmre <emrethedev@gmail.com>
2023-10-06 02:06:10 +0300

    bloc test added

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,14 +1,53 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:number_trivia/core/util/input_converter.dart';
+
+import '../../domain/entities/number_trivia.dart';
+import '../../domain/usecases/get_concrete_number_trivia.dart';
+import '../../domain/usecases/get_random_number_trivia.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be a positive integer or zero';
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial()) {
-    on<NumberTriviaEvent>((event, emit) {
-      // TODO: implement event handler
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    required GetConcreteNumberTrivia concrete,
+    required GetRandomNumberTrivia random,
+    required this.inputConverter,
+  })  : getConcreteNumberTrivia = concrete,
+        getRandomNumberTrivia = random,
+        super(
+          NumberTriviaInitial(),
+        ) {
+    on<GetTriviaForConcreteNumber>((event, emit) async {
+      final inputEither =
+          inputConverter.stringToUnsignedInteger(event.numberString);
+
+      inputEither.fold(
+        (failure) {
+          return emit(
+            const Error(
+              message: INVALID_INPUT_FAILURE_MESSAGE,
+            ),
+          );
+        },
+        (success) {
+          return emit(
+            const Loaded(
+              numberTrivia: NumberTrivia(number: 1, text: 'text'),
+            ),
+          );
+        },
+      );
     });
   }
 }
---
+++ test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
@@ -0,0 +1,105 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/util/input_converter.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+
+import 'number_trivia_bloc_test.mocks.dart';
+
+@GenerateNiceMocks(
+  [
+    MockSpec<GetConcreteNumberTrivia>(),
+    MockSpec<GetRandomNumberTrivia>(),
+    MockSpec<InputConverter>(),
+  ],
+)
+void main() {
+  late MockGetConcreteNumberTrivia mockGetConcreteNumberTrivia;
+  late MockGetRandomNumberTrivia mockGetRandomNumberTrivia;
+  late MockInputConverter mockInputConverter;
+  late NumberTriviaBloc bloc;
+
+  setUp(
+    () {
+      mockGetConcreteNumberTrivia = MockGetConcreteNumberTrivia();
+      mockGetRandomNumberTrivia = MockGetRandomNumberTrivia();
+      mockInputConverter = MockInputConverter();
+      bloc = NumberTriviaBloc(
+        concrete: mockGetConcreteNumberTrivia,
+        random: mockGetRandomNumberTrivia,
+        inputConverter: mockInputConverter,
+      );
+    },
+  );
+
+  test(
+    'initialState should be NumberTriviaInitial',
+    () {
+      expect(
+        bloc.state,
+        equals(
+          NumberTriviaInitial(),
+        ),
+      );
+    },
+  );
+  group(
+    'GetTriviaForConcreteNumber',
+    () {
+      final tNumberString = '1';
+      final tNumberParsed = 1;
+      final tNumberTrivia = const NumberTrivia(number: 1, text: 'test trivia');
+
+      test(
+        '''should call the InputConverter to validate and
+         convert the string to an unsigned integer''',
+        () async {
+          // arrange
+          when(mockInputConverter.stringToUnsignedInteger(any)).thenReturn(
+            Right(tNumberParsed),
+          );
+          // act
+          bloc.add(GetTriviaForConcreteNumber(tNumberString));
+          await untilCalled(
+            mockInputConverter.stringToUnsignedInteger(any),
+          );
+          // assert
+          verify(
+            mockInputConverter.stringToUnsignedInteger(tNumberString),
+          );
+        },
+      );
+      test(
+        'should emit [Error] when the input is invalid',
+        () async {
+          // arrange
+          when(mockInputConverter.stringToUnsignedInteger(any)).thenReturn(
+            Left(
+              InvalidInpuFailure(),
+            ),
+          );
+
+          // assert later
+          final expected = [
+            NumberTriviaInitial(),
+            const Error(
+              message: INVALID_INPUT_FAILURE_MESSAGE,
+            ),
+          ];
+          expectLater(
+            bloc.state,
+            emitsInOrder(
+              expected,
+            ),
+          );
+          // act
+          bloc.add(GetTriviaForConcreteNumber(tNumberString));
+        },
+      );
+    },
+  );
+}
